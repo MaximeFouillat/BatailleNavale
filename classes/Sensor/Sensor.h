aa6370246d5e1642b02c876c57aaaf608f6ce71e
MaximeFouillat <31452339+MaximeFouillat@users.noreply.github.com>
2018-03-08 19:48:55 +0100

    Update Sensor.h
    Sensor header created

--- classes/Sensor/Sensor.h
+++ classes/Sensor/Sensor.h
@@ -1,1 +1,17 @@
+/*
+  Sensor.h - Library for flashing Sensor code.
+*/
+#ifndef Sensor_h
+#define Sensor_h
 
+#include "Arduino.h"
+
+class Sensor
+{
+  public:
+    Sensor(int in_pin_sensor);
+    int read_sensor(int in_pin_sensor);
+  private:
+    int _in_pin_sensor; // sensor's output : 60 sensors in the game
+    int _is_shooter = HIGH;  // HIGH MEANS NO Shooter
+}
