f435d031db78d3b4ea20ee65153cd1f6c3deaa8c
MaximeFouillat <31452339+MaximeFouillat@users.noreply.github.com>
2018-03-08 19:49:41 +0100

    Update Sensor.cpp
    Sensor source code added

--- classes/Sensor/Sensor.cpp
+++ classes/Sensor/Sensor.cpp
@@ -1,1 +1,23 @@
+#include "Arduino.h"
+#include "Sensor.h"
 
+Sensor::Sensor(int in_pin_sensor){
+  pinMode(pin, INPUT);
+  _in_pin_sensor = in_pin_sensor;
+  _is_shooter = HIGH;
+}
+
+int Sensor::read_sensor()
+{
+  _is_shooter = digitalRead(_in_pin_sensor);
+  if (_is_shooter == LOW)
+  {
+    Serial.println("SHOOTER PRESENT!!");
+  }
+  else
+  {
+    Serial.println("clear");
+  }
+  delay(200);
+  return _is_shooter;
+}  
